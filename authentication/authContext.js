8989383f2e54e99c771253c4a145872a5dc9796a
burlesquel <batumanav@gmail.com>
2022-02-13 18:22:41 +0300

    contexts

---
+++ authentication/authContext.js
@@ -0,0 +1,38 @@
+import { createContext, useState } from "react"
+import netlifyIdentity from "netlify-identity-widget"
+
+const AuthContext = createContext({
+    user: null,
+    login: () => { },
+    logout: () => { },
+    authReady: false
+})
+
+export const AuthContextProvider = ({ children }) => {
+    const [user, setUser] = useState(null)
+
+    useEffect(()=>{
+        // init netlify identity connection
+        netlifyIdentity.init()
+    },[])
+
+    const login = ()=>{
+        netlifyIdentity.open()
+    }
+
+    const context = {
+        user:user,
+        login: login,
+        logout: null,
+        authReady:false
+
+    }
+
+    return (
+        <AuthContext.Provider value={context}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
+
+export default AuthContext
--- components/Layout.js
+++ components/Layout.js
@@ -3,24 +3,35 @@ import Link from "next/link"
 import Image from "next/image"
 import styles from "../styles/Layout.module.css"
 
+import AuthContext from "../authentication/authContext"
+import { useContext, useEffect, useState } from "react"
+
+
 export default function Layout({ children }) {
+
+    const value = useContext(AuthContext) // determined value in authcontext provider as props
+    console.log(value);
+    
     return (
         <section>
+
             <navbar className={styles.navbar}>
                 <div className={styles.logodiv}>
-                    <Image alt="image" className={styles.logo} src={"https://i.ibb.co/nDmVdY3/Logo-withoutbg-blackwithtext.png"} layout="responsive" width={250} height={50}/>
+                    <Image alt="image" className={styles.logo} src={"https://i.ibb.co/nDmVdY3/Logo-withoutbg-blackwithtext.png"} layout="responsive" width={250} height={50} />
                 </div>
                 <div className={styles.linksdiv}>
-                <Link href={"/"}><h3>Index</h3></Link>
-                <Link href={"/products"}><h3>Products</h3></Link>
-                <Link href={"/new_product"}><h3>Create New Product</h3></Link>
+                    <Link href={"/"}><h3>Index</h3></Link>
+                    <Link href={"/products"}><h3>Products</h3></Link>
+                    <Link href={"/new_product"}><h3>Create New Product</h3></Link>
                 </div>
-                
             </navbar>
+
             {children}
+
             <footer className={styles.footer}>
                 FOOTER
             </footer>
+
         </section>
     )
 }
