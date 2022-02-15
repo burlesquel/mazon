16244810c520755e877a7a5aa347baf922f425a6
burlesquel <batumanav@gmail.com>
2022-02-15 16:47:41 +0300

    registered first config

--- netlify/functions/identity-signup.js
+++ netlify/functions/identity-signup.js
@@ -1,7 +1,32 @@
+import axios from "axios"
+
+class User{
+    constructor(id, email, name, role, confirmation_done_at){
+        this.id = id
+        this.email = email
+        this.name = name
+        this.role = role
+        this.confirmation_done_at = confirmation_done_at
+        this.stores = []
+
+    }
+}
+
+
+const addUser = async (user) => {
+    const newUser = new User(user.id, user.email, user.user_metadata.full_name, user.role, user.confirmation_sent_at)
+    // const res = await axios({ // ADD NEW USER TO DATABASE WITH SENDING POST REQUEST TO MAIN SERVER
+    //     method: 'post',
+    //     url: 'https://mazon-server.herokuapp.com/adduser',
+    //     data: newUser
+    // })
+    console.log(newUser);
+}
+
 
 
 export async function handler(event, context) {
-    import { addUser } from "../../data-management/addUser";
+    
     try {
         const { user } = JSON.parse(event.body)
         console.log(user);
