a85023db5ded6dd64593b9105364963c427eff82
burlesquel <batumanav@gmail.com>
2022-02-16 04:12:41 +0300

    subcategory

--- components/DropdownMenu.js
+++ components/DropdownMenu.js
@@ -1,7 +1,7 @@
 import styles from "../styles/DropdownMenu.module.css"
 import { useState } from 'react'
 import Link from "next/link"
-
+var key = 0
 export default function DropDown({ title, links }) {  // links = [{name:"name", href:"href"}]
   const [open, setOpen] = useState(false)
   return (
@@ -15,8 +15,9 @@ export default function DropDown({ title, links }) {  // links = [{name:"name", 
       {open ? <div className={styles.dropdown}>
 
           {links.map((item)=>{
+            key = key+1
             return(
-              <Link href={item.href}>{item.name}</Link>
+              <Link key={key} href={item.href}>{item.name}</Link>
             )
           })}
         
